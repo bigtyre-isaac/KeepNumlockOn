248a9eb5e205ff730f84aa71b41a7f7d8dbe7b83
Isaac <isaac@bigtyre.com.au>
2023-08-24 14:24:35 +1000

    Add project files.

---
+++ KeepNumlockOn/Program.cs
@@ -0,0 +1,22 @@
+﻿using WindowsInput;
+using WindowsInput.Native;
+
+var inputSimulator = new InputSimulator();
+
+Console.WriteLine("NumLock will be kept on. Press Ctrl+C stop the program...");
+while (true)
+{
+    bool isNumLockOn = Console.NumberLock;
+
+    if (!isNumLockOn)
+    {
+        inputSimulator.Keyboard.KeyPress(VirtualKeyCode.NUMLOCK);
+        Console.WriteLine("Num Lock has been turned on.");
+    }
+    else
+    {
+        // Console.WriteLine("Num Lock is already on.");
+    }
+
+    Thread.Sleep(500); // Wait until the next iteration
+}
